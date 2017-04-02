b6e350ac7b8f83917939c19c10bdbe4a2b3738b0
Prince Kumar <prince.prakashsao@gmail.com>
2017-04-02 19:50:46 +0000

    added ORDER and CUSTOMER class and start of main

--- chef.cpp
+++ chef.cpp
@@ -1,25 +1,56 @@
 //header files
 #include<iostream>
-#include<lists>
+#include<ctype>
+#include<algorithm>
 
 //classes
-class CUSTOMER{
+class ORDER{
+    public:
+    int order_count;
+    char* food[order_count];
+    int cost[order_count];
+    int time[order_count];
+};
+
+
+class CUSTOMER:public ORDER{
+     int static count;
+     int c_num; //customer_number
+     ORDER order;
+     public:
+     CUSTOMER(){
+         c_num = count++;
+     }
+     CUSTOMER(int/*count of orders*/,char*[]/*array of orders*/);
+};
+
+
+//class constructors definations
+CUSTOMER::CUSTOMER(int order_count, char* order_array[order_count]){
+    c_num = count++;
     
 }
 
-
-
-//functions
+//class functions
+//normal functions
 
 
 //main
 int main(){
+    char* food;
+    int order_count;
+    char* order_array[];
+    
     //get the no of customers, say 'n'
     int no_of_customers;
     cout<<"Number of customers: ";
     cin>>no_of_customers;
     
-    //create 'n' of customer objects
+    //create 'n' number of customer objects
+    for(int i=0; i<no_of_customers; i++){
+        CUSTOMERS c[i];
+    }
     
-    
+    cout<<"Enter the names of the food items and enter 'DONE' if no more\n";
+    while(tolower(food) != 'done')
 }
