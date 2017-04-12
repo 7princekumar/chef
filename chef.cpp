a8812b087b41b38d2624803c2bfb9c99586d4e84
Prince Kumar <prince.prakashsao@gmail.com>
2017-04-12 00:14:47 +0000

    added customer no in chef output

--- chef.cpp
+++ chef.cpp
@@ -1,4 +1,15 @@
-//header files
+/*//////////////////////////PROGRAM DETAILS///////////////////////
+                     OOP OPEN ENDED PROJECT, 2017
+              SIDDAGANGA INSTITUTE OF TECHNOLOGY, TUMKUR
+                
+            NAME OF THE PROJECT :::: Job management of Chefs.
+                
+                      PROJECT SUBMITTED BY: 
+                  1. Prince Kumar - 1SI15CS080
+                  2. Puru Jaiswal - 1SI15CS081
+                
+             PROJECT SUBMITTED TO ::: Prof. H.D. Kallinatha
+///////////////////////////HEADER FILES//////////////////////////*/
 #include<iostream>
 #include<cctype>
 #include<vector>
@@ -9,29 +20,24 @@
 #define NO_OF_CHEF 5
 using namespace std;
 
+
+
+///////////////////////////////NODE////////////////////////////////
 struct node{
     string food;
     int cost;
     int time;
+    int customer_num;
 };
 typedef struct node NODE;
 
 
-//functions prototypes
-int menu_cost(string);
-int menu_time(string);
-int max_of_vector(vector<NODE>);
-bool compare(NODE,NODE);
 
-
-
-
-//classes
+/////////////////////////////classes///////////////////////////////
 class ORDER{
     public:
     vector<NODE>order_node;
 };
-
 
 class CUSTOMER:public ORDER{
      int static count;
@@ -46,23 +52,28 @@ class CUSTOMER:public ORDER{
      }
 };
 
-
 class CHEF:public CUSTOMER{
      public:
      vector<NODE>job;
      int work_time;
+    // vector<int>customer_min_time;
 };
 
 
-CHEF& chef_with_min_work_time(vector<CHEF>& , NODE); //return by reference concept//fucntion prototype
+
+//////////////////////functions prototypes/////////////////////////
+int menu_cost(string);
+int menu_time(string);
+bool compare(NODE,NODE);
+CHEF& chef_with_min_work_time(vector<CHEF>& , NODE); 
 CHEF& chef_with_min_work_time(vector<CHEF>&, int, NODE);
 int CUSTOMER::count; //static count
 
 
 
-//main
+/////////////////////////////main/////////////////////////////////
 int main(){
-    int i,j;
+    int i,j,k=0;
     string name;
     NODE item;
     vector<NODE> order_array;
@@ -71,28 +82,29 @@ int main(){
     int no_of_customers;
     cout<<"Number of customers: ";
     cin>>no_of_customers;
-
-    //create 'n' number of customer objects and initialise em
+    
+    //create 'n' number of customer objects
     vector<CUSTOMER> c;
-    c.resize(no_of_customers); //resolves the variable length array problem
+    c.resize(no_of_customers);
 
+    ///////////INPUT - get orders from all customers /////////////////
+    cout<<"\n\nENTER THE NAMES OF THE FOOD ITEMS AND ENTER 'done' IF NO MORE\n";
     for(i=0; i<no_of_customers; i++){
         //reset item and order_array as we need to reuse it
         name = "";
         order_array.clear();
-
-        cout<<"CUSTOMER: ["<<i<<"] ::\n";
-        cout<<"Enter the names of the food items and enter 'done' if no more\n";
+        
+        cout<<"\n           CUSTOMER : ["<<i<<"]\n";
         while(1){
+            cout<<"           : ";
             cin>>name;
             if(name == "done") break;
             item.food = name;
             item.cost = menu_cost(name);
             item.time = menu_time(name);
-
-            order_array.push_back(item); /////
+            item.customer_num = i;
+            order_array.push_back(item);
         }
-
         //put the values inside each customer data
         c[i].order_node = order_array;
     }
@@ -100,25 +112,27 @@ int main(){
 
     //create instance of chefs
     vector<CHEF> chef;
-    chef.resize(NO_OF_CHEF); ////////FIX IT IN FUTURE, let there be any chef for now
+    chef.resize(NO_OF_CHEF); 
     for(i=0; i<NO_OF_CHEF; i++)
         chef[i].work_time = 0;
     
 
-    //ALGO
+/////////////...........PROCESSING - ALGORITHM............///////////////
     //1. SORT THE NODE ARRAY in decreasing order
     for(i = 0; i<no_of_customers; i++){
-        sort(c[i].order_node.begin(), c[i].order_node.end(), compare);  //order_node is now sorted
-        for(j=0; j<c[i].order_node.size(); j++)
-            cout<<c[i].order_node[j].time <<" -> ";
-        cout<<endl;
+        sort(c[i].order_node.begin(), c[i].order_node.end(), compare);
+        // for(j=0; j<c[i].order_node.size(); j++)
+        //     cout<<c[i].order_node[j].time <<" -> ";
+        // cout<<endl;
     }
 
     //2. Push it to chef accordingly
     int spare_max = 0;
     for(i=0; i<no_of_customers; i++){
-         order_array = c[i].order_node; //order_array is sorted list of orders of customer[i]
-         for(j=0; j<order_array.size(); j++){ //iterate over this array and assign its order to chefs accordingly
+         order_array = c[i].order_node; 
+         //order_array is sorted list of orders of customer[i]
+         //iterate over this array and assign its order to chefs accordingly
+         for(j=0; j<order_array.size(); j++){ 
              spare_max = order_array[0].time; //since sorted in decreasing order
              
              //for first order
@@ -128,7 +142,8 @@ int main(){
              //for other orders
              while(j<= (order_array.size())){
                  if(j+1 > order_array.size()) break;
-                 chef_with_min_work_time(chef, spare_max, order_array[j]).job.push_back(order_array[j]);
+                 chef_with_min_work_time
+                      (chef, spare_max, order_array[j]).job.push_back(order_array[j]);
                  if(j == order_array.size()) break;
                  j++;
              }
@@ -138,25 +153,34 @@ int main(){
 
 
 
-    //testing the order's list of customers by printing
+///////......... OUTPUT - LISTS OF CUSTOMERS and THEIR ORDERS .........///////// 
     for(i=0; i<no_of_customers; i++){
-        cout<<"Customer ["<<i<<"] :\n";
-        cout<<setw(20)<<"Name "<<setw(20)<<"  Time"<<setw(20)<<"Cost\n";
+        cout<<"\n\t                 CUSTOMER : ["<<i<<"]\n";
+        cout<<"\t\t  "<<setw(9)<<"Name "<<setw(8)<<" Time"<<setw(9)<<"Cost\n";
         for(j=0; j<c[i].order_node.size(); j++){
-            cout<<setw(20)<<c[i].order_node[j].food<<setw(20)<<c[i].order_node[j].time<<setw(20)<<c[i].order_node[j].cost<<endl;
+            cout<<"\t\t"<<setw(10)<<c[i].order_node[j].food<<setw(8)<<
+            c[i].order_node[j].time<<setw(8)<<c[i].order_node[j].cost<<endl;
         }
         cout<<endl;
     }
 
-    //testing the job list of chef by printing
+/////....... OUTPUT - LISTS OF CHEF and THEIR WORK DISTRIBUTION ......./////// 
     for(i=0; i<chef.size(); i++){
-        cout<<"CHEF ["<<i<<"] :  ";
+        cout<<"\n             CHEF["<<i<<"] :  ";
         //cout<<"Name \t\tTime \tCost\n";
         for(j=0; j<chef[i].job.size(); j++){
-            cout<<chef[i].job[j].time<<" -> ";
+            cout<<"c"<<chef[i].job[j].customer_num<<"_"<<chef[i].job[j].time<<" -> ";
         }
         cout<<endl;
     }
+    
+    
+    // for(i=0; i<no_of_customers; i++){
+    //     int max_time = 
+    //     for(j=0; j<chef.size(); j++){
+            
+    //     }
+    // }
 
 
     return 0;
@@ -167,7 +191,7 @@ int main(){
 
 
 
-//normal functions
+/////////////////////////function definations///////////////////////////
 int menu_time(string sample){
     //input: one order name as string
     //output: time taken to complete that order as integer(mins)
@@ -175,15 +199,15 @@ int menu_time(string sample){
      if (sample == "dosa")   return 30;
      if (sample == "tea")    return 5;
      if (sample == "samosa") return 7;
-     if (sample == "samosa2") return 7;
      if (sample == "coffee") return 5;
      if (sample == "noodle") return 20;
      if (sample == "friedrice") return 25;
+     return 0;
      //more to be added
 }
 
 int menu_cost(string sample){
-    //input: one order name as string
+    //input:  one order name as string
     //output: cost of that order as integer
      if (sample == "idli")   return 7;
      if (sample == "dosa")   return 30;
@@ -192,26 +216,25 @@ int menu_cost(string sample){
      if (sample == "coffee") return 8;
      if (sample == "noodle") return 60;
      if (sample == "friedrice") return 30;
+     return 0;
      //more to be added
 }
 
-int max_of_vector(vector<NODE> V){
-    int i;
-    int max_ = V[0].time;
-    for(i=1; i<V.size(); i++){
-        if ( V[i].time > max_ ) max_ = V[i].time;
-    }
-    return max_;
-}
+
 
 
 bool compare(NODE a,NODE b){
+    //input:  two order structures of type NODE
+    //output: returns boolean, True: if NODE a order time is greater
+    //        otherwise false
     return (a.time > b.time);
 }
 
 
 
 CHEF& chef_with_min_work_time(vector<CHEF> &a, NODE order_to_be_pushed){
+    //input:  vector of chefs, struct of an order
+    //output: reference to a chef with min work_time
     int min_pos = 0;
     for(int i=0; i<a.size(); i++){
         if(a[i].work_time < a[min_pos].work_time)
@@ -224,6 +247,8 @@ CHEF& chef_with_min_work_time(vector<CHEF> &a, NODE order_to_be_pushed){
  
  
 CHEF& chef_with_min_work_time(vector<CHEF> &a, int spare_max, NODE order_to_be_pushed){
+    //input:  vector of chefs, maxTimeTaking order, struct of an order
+    //output: reference to a chef which satifies certain conditions
      int min_pos = 0;
      for(int i=0; i<a.size(); i++){
          if((a[i].work_time + order_to_be_pushed.time) <= spare_max){
@@ -231,11 +256,9 @@ CHEF& chef_with_min_work_time(vector<CHEF> &a, int spare_max, NODE order_to_be_p
               break;
          }
      }
-     if(min_pos == 0){ //i.e, no chef that satisfies this condition, so return chef with min worktime only
+     if(min_pos == 0) //i.e, no chef that satisfies this condition, so return chef with min worktime only
          return chef_with_min_work_time(a,order_to_be_pushed);
-     }
-     else{
+     else
          a[min_pos].work_time += order_to_be_pushed.time;
-    }
     return a[min_pos];
 }
